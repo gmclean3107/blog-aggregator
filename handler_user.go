62f06c773a4ce661ad4e516c4e27522181d29ff5
gmclean3107 <gman3107@outlook.com>
2026-09-12 03:47:31 +0100

    Merged the login and registration handlers into the same user handler class.

--- handler_user.go
+++ handler_user.go
@@ -34,13 +34,38 @@ func handlerRegister(s *State, cmd Command) error {
 	}
 
 	fmt.Println("User created successfully")
-	fmt.Printf("User Data:\n%v", user)
+	fmt.Printf("User Data:\nID: %v\nName: %s\n", user.ID, user.Name)
 
 	err = s.cfg.SetUser(cmd.args[0])
 
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func handlerLogin(s *State, cmd Command) error {
+
+	if len(cmd.args) == 0 {
+		return errors.New("Must supply username")
+	}
+
+	if len(cmd.args) > 1 {
+		return errors.New("Only supply one username with login")
+	}
+
+	if _, err := s.db.GetUser(context.Background(), cmd.args[0]); err != nil {
+		return err
+	}
+
+	err := s.cfg.SetUser(cmd.args[0])
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Username has been set to: %s\n", cmd.args[0])
 
 	return nil
 }
